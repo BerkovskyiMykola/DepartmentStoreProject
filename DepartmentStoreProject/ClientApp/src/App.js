8033b4cbe41222a9013e55249c964a66507d8069
Mykola Berkovskyi <nomvodafon@gmail.com>
2021-12-24 02:20:39 +0200

    Some changes

--- DepartmentStoreProject/ClientApp/src/App.js
+++ DepartmentStoreProject/ClientApp/src/App.js
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import { Router, Switch, Route, Link } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css'
+import { Router, Switch, Route, NavLink as RRNavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -16,16 +14,20 @@ import Shop from "./components/Shop/Shop";
 import ShopItem from "./components/ShopItem/ShopItem";
 import History from "./components/History/History";
 
+import './App.css'
+
+import { Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, Collapse, NavLink, DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown, Container } from "reactstrap";
+
 import { logout } from "./actions/auth";
 import { clearMessage } from "./actions/message";
 
 import { history } from './utils/history';
 import EventBus from "./common/EventBus";
-import { useTranslation } from "react-i18next";
 
 export default function App() {
     const { t, i18n } = useTranslation();
     const dispatch = useDispatch();
+    const [isOpen, setIsOpen] = useState(false);
 
     const { user } = useSelector(state => ({
         user: state.auth.user,
@@ -51,70 +53,67 @@ export default function App() {
     return (
         <Router history={history}>
             <div>
-                <nav className="navbar navbar-expand navbar-dark bg-dark">
-                    <Link to={"/"} className="navbar-brand">
+                <Navbar
+                    color="dark"
+                    dark
+                    expand="md"
+                    light
+                >
+                    <NavbarBrand tag={RRNavLink} exact to="/">
                         DepartmentStoreProject
-                    </Link>
-
-                    <div className="navbar-nav mr-auto">
-                        <li className="nav-item">
-                            <a href="/" className="nav-link" onClick={(e) => { e.preventDefault(); i18n.changeLanguage("en"); }}>
-                                EN
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/" className="nav-link" onClick={(e) => { e.preventDefault(); i18n.changeLanguage("ua"); }}>
-                                UA
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <Link to={"/home"} className="nav-link">
-                                {t("Home")}
-                            </Link>
-                        </li>
-                    </div>
-
-                    <div className="navbar-nav ml-auto">
-                        {user ? (
-                            <><li className="nav-item">
-                                <Link to={"/profile"} className="nav-link">
-                                    {t("Profile")}
-                                </Link>
-                            </li>
-                                {user.role === "Admin" &&
+                    </NavbarBrand>
+                    <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    {i18n.language.toUpperCase()}
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem onClick={(e) => i18n.changeLanguage("ua")}>
+                                        UA
+                                    </DropdownItem>
+                                    <DropdownItem onClick={(e) => i18n.changeLanguage("en")}>
+                                        EN
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                        <Nav className="ml-auto" navbar>
+                            {user ? (
+                                <><NavItem>
+                                    <NavLink tag={RRNavLink} exact to="/profile">{t("Profile")}</NavLink>
+                                </NavItem>
+                                    {user.role === "Admin" &&
+                                        <NavItem>
+                                            <NavLink tag={RRNavLink} exact to="/users">{t("Users")}</NavLink>
+                                        </NavItem>
+                                    }
+                                    {user.role === "User" &&
+                                        <NavItem>
+                                            <NavLink tag={RRNavLink} exact to="/departmentStores">{t("departmentStores")}</NavLink>
+                                        </NavItem>
+                                    }
                                     <li className="nav-item">
-                                        <Link to={"/users"} className="nav-link">
-                                            {t("Users")}
-                                        </Link>
-                                    </li>
-                                }
-                                {user.role === "User" &&
-                                    <li className="nav-item">
-                                        <Link to={"/departmentStores"} className="nav-link">
-                                            {t("departmentStores")}
-                                        </Link>
-                                    </li>
-                                }
-                                <li className="nav-item">
-                                    <a href="/login" className="nav-link" onClick={logOut}>
-                                        {t("LogOut")}
-                                    </a>
-                                </li></>
-                        ) : (
-                            <><li className="nav-item">
-                                <Link to={"/login"} className="nav-link">
-                                    {t("Login")}
-                                </Link>
-                            </li><li className="nav-item">
-                                <Link to={"/register"} className="nav-link">
-                                    {t("SignUp")}
-                                </Link>
-                            </li></>
-                        )}
-                    </div>
-                </nav>
+                                        <a href="/login" className="nav-link" onClick={logOut}>
+                                            {t("LogOut")}
+                                        </a>
+                                    </li></>
+                            ) : (
+                                <>
+                                    <NavItem>
+                                        <NavLink tag={RRNavLink} exact to="/login">{t("Login")}</NavLink>
+                                    </NavItem>
+                                    <NavItem>
+                                        <NavLink tag={RRNavLink} exact to="/register">{t("SignUp")}</NavLink>
+                                    </NavItem>
+                                </>
+                            )}
+                        </Nav>
+                    </Collapse>
+                </Navbar>
 
-                <div className="container mt-3">
+                <div className="mt-3">
                     <Switch>
                         <Route exact path={["/", "/home"]} component={Home} />
                         <Route exact path="/login" component={Login} />
--- DepartmentStoreProject/ClientApp/src/index.js
+++ DepartmentStoreProject/ClientApp/src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
